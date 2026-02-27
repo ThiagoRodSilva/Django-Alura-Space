6abf50d9f5757da27657dbdcc339253702aedc2a
Thiago4596 <116847445+Thiago4596@users.noreply.github.com>
2026-02-27 17:59:06 -0300

    Atualiza o script de build para usar Python 3.14 e corrige a configuração do runtime no vercel.json

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,11 @@
 echo "BUILD START"
 
 # Instala as dependências
-python3.9 -m pip install -r requirements.txt
+python3.14 -m pip install -r requirements.txt
 
 # Faz as migrações e coleta os arquivos estáticos
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput --clear
+python3.14 manage.py makemigrations --noinput
+python3.14 manage.py migrate --noinput
+python3.14 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
