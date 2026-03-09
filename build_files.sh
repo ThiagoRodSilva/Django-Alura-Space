1fd936a80c9fcc57d5130c28ba93f151d956e958
ThiagoRodSilva <trsilva15544@gmail.com>
2026-03-09 10:02:42 -0300

    feat: Implement comprehensive Django settings for S3 storage and configure Vercel for static file collection.

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-set -e  # para o script se algo falhar
-
-echo "📦 Instalando dependências..."
 pip install -r requirements.txt
-
-echo "🔄 Rodando migrações..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
-echo "🎨 Coletando estáticos..."
 python manage.py collectstatic --noinput
-
-echo "✅ Build concluído!"
