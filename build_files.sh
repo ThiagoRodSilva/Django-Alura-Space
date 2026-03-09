a26d9fb16c1a3a5619f4e2a91929995f851e53c2
ThiagoRodSilva <trsilva15544@gmail.com>
2026-03-09 10:06:02 -0300

    build: Explicitly use python3.12 for pip and collectstatic, and add --break-system-packages to pip install.

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3.12 -m pip install -r requirements.txt --break-system-packages
+python3.12 manage.py collectstatic --noinput
